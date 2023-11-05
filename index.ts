dfa5a6503fe0d6ae4a685c3d702331ad291ea15c
islom-sattorov <slmsttrv1@gmail.com>
2023-11-05 15:05:43 +0500

    add: ts

--- index.ts
+++ index.ts
@@ -1,31 +1,30 @@
-const puppeteer = require("puppeteer");
+import { Browser } from "puppeteer";
+
 const puppeteerExtra = require("puppeteer-extra");
 const RecaptchaPlugin = require("puppeteer-extra-plugin-recaptcha");
-require('dotenv').config();
-
+require("dotenv").config();
 
 const websiteURL = process.env.WEBSITE;
-const userEmail = process.env.EMAIL;
-const userPassword = process.env.PASSWORD;
+const userEmail = process.env.EMAIL ?? "";
+const userPassword = process.env.PASSWORD ?? "";
 const capId = process.env.CAP_ID;
 const capToken = process.env.CAP_TOKEN;
-
 
 puppeteerExtra.use(
   RecaptchaPlugin({
     provider: {
       id: capId,
-      token: capToken
+      token: capToken,
     },
-    visualFeedBack: true
+    visualFeedBack: true,
   })
 );
 
-
 (async () => {
-  const browser = await puppeteerExtra.launch({ headless: false });
+  const browser: Browser = await puppeteerExtra.launch({ headless: false });
+
   const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080 })
+  await page.setViewport({ width: 1920, height: 1080 });
 
   await page.goto(`${websiteURL}/auth`);
   await page.waitForSelector(".mdl-button");
@@ -33,17 +32,14 @@ puppeteerExtra.use(
   await page.type("#email", userEmail);
   await page.type("#password", userPassword);
 
-
   const { solved, error } = await page.solveRecaptchas();
   if (solved) {
-    console.log("Solved", solved)
+    console.log("Solved", solved);
   } else {
-    console.log("Not solved", error)
+    console.log("Not solved", error);
   }
 
-  const btn = await page.$(".mdl-button--raised");
-
-  await btn.click();
+  await page.$(".mdl-button--raised").then(async (btn) => await btn?.click());
 
   await page.waitForNavigation();
 
@@ -59,7 +55,5 @@ puppeteerExtra.use(
   }
 
   await page.screenshot({ path: `pic.png` });
-
-
   await browser.close();
 })();
